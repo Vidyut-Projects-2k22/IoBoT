01af3281db51ed110efa48aac91b55f8d6c904cb
pranav2305 <pranav2305@gmail.com>
2022-04-08 18:06:02 +0530

    Add script for testing

---
+++ IoT/test.py
@@ -0,0 +1,90 @@
+# enable TLS client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+
+#
+# Copyright 445021 HiveMQ GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import ssl
+import time
+import paho.mqtt.client as paho
+from paho import mqtt
+# import interact
+
+glob_otp = 0
+
+# setting callbacks for different events to see if it works, print the message etc.
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("CONNACK received with code %s." % rc)
+
+# with this callback you can see if your publish was successful
+def on_publish(client, userdata, mid, properties=None):
+    print("mid: " + str(mid))
+
+# print which topic was subscribed to
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    # interact.generateOTP("user1")
+    # otp = interact.getAuthenticationToken("user1")
+    # global glob_otp
+    # glob_otp = otp
+    # client.publish("encyclopedia/test.", otp, qos=1)
+    # print("Your OTP is: " + str(otp))
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg):
+    # msg = msg.payload.decode("utf-8")
+    # if msg == "generate":
+    #     interact.generateOTP("user1")
+    #     otp = interact.getAuthenticationToken("user1")
+    #     client.publish("encyclopedia/test.", "Your OTP is : " + str(otp), qos=1)
+    # elif (len(msg) == 6):
+    #     res = interact.validateOTP(otp, "user1")
+    #     if not res:
+    #         print("Invalid OTP")
+    #         client.publish("encyclopedia/test", "Invalid OTP", qos=1)
+    #         client.disconnect()
+    #     else:
+    #         print("Valid OTP")
+    #         client.publish("encyclopedia/test", "Valid OTP", qos=1)
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
+# userdata is user defined data of any type, updated by user_data_set()
+# client_id is the given name of the client
+client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+client.on_connect = on_connect
+
+# enable TLS for secure connection
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+# set username and password
+client.username_pw_set("Prasanna8446", "Prasu8446")
+# connect to HiveMQ Cloud on port 8883 (desult for MQTT)
+client.connect("64a80fb731404d588892b35b24a09263.s1.eu.hivemq.cloud", 8883)
+
+
+# setting callbacks, use separate functions like above for better visibility
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.on_publish = on_publish
+
+
+# subscribe to all topics of encyclopedia by using the wildcard "#"
+client.subscribe("encyclopedia/#", qos=1)
+client.publish("encyclopedia/test", "348446", qos=1)
+
+# a single publish, this can also be done in loops, etc.
+
+# loop_forever for simplicity, here you need to stop the loop manually
+# you can also use loop_start and loop_stop
+client.loop_forever()
